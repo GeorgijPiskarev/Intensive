a6cf04e09909af7816bcd79fd85c981f63654c27
Georgij Piskarev <georgij5121@gmail.com>
2021-03-26 02:18:44 +0300

    final test

--- src/test/java/web/UserTest.java
+++ src/test/java/web/UserTest.java
@@ -1,7 +1,6 @@
 package web;
 
 import model.User;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
@@ -21,12 +20,11 @@ import static web.UserTestData.*;
 @SpringJUnitConfig(locations = {
         "classpath:spring/spring-app.xml"
 })
-//@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class UserTest {
     @Autowired
     private UserController controller;
 
-    @Disabled
     @Test
     public void create() {
         User created = controller.create(getNew());
@@ -35,37 +33,36 @@ public class UserTest {
         newUser.setId(newId);
         assertThat(created).isEqualTo(newUser);
     }
-    @Disabled
+
     @Test
     public void delete() {
         controller.delete(FIRST_ID);
         assertThrows(NotFoundException.class, () -> controller.get(FIRST_ID));
     }
-    @Disabled
+
     @Test
     public void deletedNotFound() {
         assertThrows(NotFoundException.class, () -> controller.delete(NOT_FOUND));
     }
 
-    @Disabled
     @Test
     public void get() {
         User user = controller.get(FIRST_ID);
         assertThat(user).isEqualTo(user1);
     }
-    @Disabled
+
     @Test
     public void getNotFound() {
         assertThrows(NotFoundException.class, () -> controller.get(NOT_FOUND));
     }
-    @Disabled
+
     @Test
     public void update() {
         User updated = getUpdated();
         controller.update(updated, updated.getId());
         assertThat(controller.get(FIRST_ID)).isEqualTo(updated);
     }
-    @Disabled
+
     @Test
     public void getAll() {
         List<User> all = controller.getAll();
